610f6ade52deca9d9c234946fb2a98ca4b064506
202416 <202416@twfhclife.com.tw>
2017-09-05 10:36:45 +0800

    20170905 10:35 finish IndexMAP-calculateMAP method 1st version.

--- stocks-modules/src/main/java/tw/idv/earvin/stockpgms/stocks_modules/indexes/IndexMAP.java
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/stocks_modules/indexes/IndexMAP.java
@@ -7,36 +7,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class IndexMAP {
-	
+
 	/**
 	 * Calculate the average value of volume and main value.
-	 * @param stockNo 股票代號
+	 * 
+	 * @param stockNo
+	 *            股票代號
 	 */
 	public static StocksData[] calculateMAP(String stockNo) {
-		String SQL_1 = "SELECT COUNT(*) AS COUNTS FROM TAIWAN_DATA_POLARIS WHERE STOCK_NO = ? ";
-		String SQL_2 = "SELECT STOCK_NO, DATE, START_PRICE, HIGH_PRICE, LOW_PRICE, END_PRICE, VOLUME FROM TAIWAN_DATA_POLARIS WHERE STOCK_NO = ? ORDER BY DATE ";
-		String SQL_3 = "INSERT INTO TAIWAN_DATA_POLARIS_INDEXES_VALUES (STOCK_NO, INDEX_CODE, DATE, VALUE) VALUES (?, ?, ?, ?) ";
+		String SQL_GET_TAIWAN_DATA_POLARIS_COUNTS_BY_STOCK_NO = "SELECT COUNT(*) AS COUNTS FROM TAIWAN_DATA_POLARIS WHERE STOCK_NO = ? ";
+		String SQL_QUERY_TAIWAN_DATA_POLARIS_BY_PK = "SELECT STOCK_NO, DATE, START_PRICE, HIGH_PRICE, LOW_PRICE, END_PRICE, VOLUME FROM TAIWAN_DATA_POLARIS WHERE STOCK_NO = ? ORDER BY DATE ";
+		String SQL_QUERY_TAIWAN_DATA_POLARIS_INDEXES_VALUES_BY_PK = "SELECT * FROM TAIWAN_DATA_POLARIS_INDEXES_VALUES WHERE STOCK_NO = ? AND INDEX_CODE = ? AND DATE = ? ";
+		String SQL_UPDATE_TAIWAN_DATA_POLARIS_INDEXES_VALUES_BY_PK = "UPDATE TAIWAN_DATA_POLARIS_INDEXES_VALUES SET VALUE = ? WHERE STOCK_NO = ? AND INDEX_CODE = ? AND DATE = ? ";
+		String SQL_INSERT_TAIWAN_DATA_POLARIS_INDEXES_VALUES = "INSERT INTO TAIWAN_DATA_POLARIS_INDEXES_VALUES (STOCK_NO, INDEX_CODE, DATE, VALUE) VALUES (?, ?, ?, ?) ";
+
 		int indexDay = 5;
-		int counts = 100;
+		int counts = 0;
+		int updateCount = 0;
+		int insertCount = 0;
 		StocksData[] sd = null;
 
 		try {
 			// 取得筆數
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/stocksdb", "root", "lin32ledi");
-			PreparedStatement pstmt = conn.prepareStatement(SQL_1);
+			PreparedStatement pstmt = conn.prepareStatement(SQL_GET_TAIWAN_DATA_POLARIS_COUNTS_BY_STOCK_NO);
 			pstmt.setString(1, stockNo);
 			ResultSet rs = pstmt.executeQuery();
 			if (rs.next()) {
 				counts = rs.getInt("COUNTS");
 			}
-			System.out.println("Total records is " + counts);
-			// 計算
+			System.out.println("作業開始，本次計算資料總筆數為 " + counts);
+			// Generate StockData object
 			sd = new StocksData[counts];
-			pstmt = conn.prepareStatement(SQL_2);
+			pstmt = conn.prepareStatement(SQL_QUERY_TAIWAN_DATA_POLARIS_BY_PK);
 			pstmt.setString(1, stockNo);
 			rs = pstmt.executeQuery();
 			for (int i = 0; i < counts; i++) {
-//				System.out.println("Total records is " + rs.getString("STOCK_NO") + ", " + rs.getInt("DATE"));
 				rs.next();
 				sd[i] = new StocksData();
 				sd[i].setStockNo(rs.getString("STOCK_NO"));
@@ -46,72 +52,89 @@ public class IndexMAP {
 				sd[i].setLowPrice(rs.getDouble("LOW_PRICE"));
 				sd[i].setEndPrice(rs.getDouble("END_PRICE"));
 				sd[i].setVolume(rs.getDouble("VOLUME"));
-				
-				System.out.println(sd[i].printData()); 				
 			}
-			
+			// Calculate the MAP
 			for (int i = 0; i < sd.length; i++) {
 				double average = 0;
 				if (i < indexDay) {
 					for (int j = 0; j <= i; j++) {
-						average += sd[i-j].getEndPrice();
+						average += sd[i - j].getEndPrice();
 					}
-					average /= (i+1);				
+					average /= (i + 1);
 				} else {
 					for (int j = 0; j < indexDay; j++) {
-						average += sd[i-j].getEndPrice();
+						average += sd[i - j].getEndPrice();
 					}
-					average /= indexDay;				
+					average /= indexDay;
 				}
 				System.out.println(sd[i].printData() + " -- the average= " + average);
-				// insert to db
-				pstmt = conn.prepareStatement(SQL_3);
+
+				// 將計算結果寫入 TAIWAN_DATA_POLARIS_INDEXES_VALUES
+				pstmt = conn.prepareStatement(SQL_QUERY_TAIWAN_DATA_POLARIS_INDEXES_VALUES_BY_PK);
 				pstmt.setString(1, sd[i].getStockNo());
 				pstmt.setInt(2, 1001);
 				pstmt.setLong(3, sd[i].getTradeDate());
-				pstmt.setDouble(4, average);
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					// update
+					pstmt = conn.prepareStatement(SQL_UPDATE_TAIWAN_DATA_POLARIS_INDEXES_VALUES_BY_PK);
+					pstmt.setDouble(1, average);
+					pstmt.setString(2, sd[i].getStockNo());
+					pstmt.setInt(3, 1001);
+					pstmt.setLong(4, sd[i].getTradeDate());
+					updateCount += 1;
+				} else {
+					// insert
+					pstmt = conn.prepareStatement(SQL_INSERT_TAIWAN_DATA_POLARIS_INDEXES_VALUES);
+					pstmt.setString(1, sd[i].getStockNo());
+					pstmt.setInt(2, 1001);
+					pstmt.setLong(3, sd[i].getTradeDate());
+					pstmt.setDouble(4, average);
+					insertCount += 1;
+				}
 				pstmt.executeUpdate();
-			}			
-
+			}
 			pstmt.close();
 			if (!conn.isClosed()) {
 				conn.close();
-			}			
-
+			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("作業完成 -- 處理股票代號= " + stockNo + ", 總筆數= " + counts + ", 新增筆數= " + insertCount + ", 更新筆數= "
+				+ updateCount);
 		return sd;
 	}
-	
+
 	/**
-	 * 更新日期：2017.08.21
-	 * 新增技術指標的值到：TAIWAN_DATA_POLARIS_INDEXES_VALUES
+	 * 更新日期：2017.08.21 新增技術指標的值到：TAIWAN_DATA_POLARIS_INDEXES_VALUES
+	 * 
 	 * @param sd
 	 */
 	public void insertMAP(StocksData[] sd) {
-		
+
 	}
-	
+
 	/**
 	 * 計算recordset筆數
-	 * @param resultSet 
+	 * 
+	 * @param resultSet
 	 * @return int 資料筆數
 	 */
 	public static int getResultSetRowCount(ResultSet resultSet) {
-	    int size = 0;
-	    try {
-	        resultSet.last();
-	        size = resultSet.getRow();
-	        resultSet.beforeFirst();
-	    }
-	    catch(Exception ex) {
-	        return 0;
-	    }
-	    return size;
+		int size = 0;
+		try {
+			resultSet.last();
+			size = resultSet.getRow();
+			resultSet.beforeFirst();
+		} catch (Exception ex) {
+			return 0;
+		}
+		return size;
 	}
+
 	public static void main(String[] args) {
-		StocksData[] sd = calculateMAP("2002");
+		// StocksData[] sd = calculateMAP("2002");
+		StocksData[] sd = calculateMAP("2349");
 	}
 }
