32209b09d6f6288f8d4d975d2b82926cf643a4d9
earvinlin <lin.earvin@gmail.com>
2017-10-04 21:17:25 +0800

    20171004 21:16 Think & write IndexData object, not finish...

--- stocks-modules/src/main/java/tw/idv/earvin/stockpgms/stocks_modules/indexes/IndexData.java
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/stocks_modules/indexes/IndexData.java
@@ -2,41 +2,23 @@ package tw.idv.earvin.stockpgms.stocks_modules.indexes;
 
 public class IndexData {
 	private String stockNo;
-	private long indexCode;
-	private long tradeDate;
-	private double value;
-//	private Connection con = null;
-	
-	
+	private String indexName;
+	private IndexValue indexValue;
+
 	public String getStockNo() {
 		return stockNo;
 	}
 	public void setStockNo(String v) {
 		stockNo = v;
 	}
-	public long getIndexCode() {
-		return indexCode;
+	public String getIndexName() {
+		return indexName;
 	}
-	public void setIndexCode(long v) {
-		indexCode = v;
-	}
-	public long getTradeDate() {
-		return tradeDate;
-	}
-	public void setTradeDate(long v) {
-		tradeDate = v;
+	public void setIndexName(String v) {
+		indexName = v;
 	}
 	
-	
-	
-	public void insert(IndexData v) {
-		
-	}
-	
-	public void delete(IndexData v) {
-		
-	}
-	
+
 	public static void main(String[] args) {
 
 	}
---
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/stocks_modules/indexes/IndexValue.java
@@ -0,0 +1,22 @@
+package tw.idv.earvin.stockpgms.stocks_modules.indexes;
+
+public class IndexValue {
+	private long date;
+	private double value;
+
+	public long getDate() {
+		return date;
+	}
+
+	public void setDate(long v) {
+		date = v;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double v) {
+		value = v;
+	}
+}
