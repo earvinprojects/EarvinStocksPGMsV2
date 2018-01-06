170d08442f3bbb2a504f0bb664022b6612fc88b4
John Lin <john.lin0908@gmail.com>
2018-01-07 07:48:38 +0800

    20180107 07:47 Write codes about draw stock form

--- stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/EarvinStocksFormTest.java
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/EarvinStocksFormTest.java
@@ -7,6 +7,20 @@ import java.awt.geom.Line2D;
 import javax.swing.*;
 
 public class EarvinStocksFormTest extends javax.swing.JFrame {
+	
+	class DisplayStocksForm extends Canvas {
+		public void paint(Graphics g) {
+			g.drawString("Hello", 140, 140);
+			setBackground(Color.WHITE);
+			g.fillRect(130, 130, 100, 80);
+			g.drawOval(130, 130, 50, 60);
+			setForeground(Color.BLUE);
+			g.fillOval(130, 130, 50, 60);
+			g.drawArc(310, 200, 40, 50, 90, 60);
+			g.fillArc(310, 130, 40, 50, 180, 40);
+
+		}
+	}
 
 	public static void main(String[] args) {
 		try {
@@ -76,20 +90,156 @@ public class EarvinStocksFormTest extends javax.swing.JFrame {
 				System.exit(0);
 			}
 		});
+		
+		//-- 20180106 ADD STR ---------------------------------
+        // 設定K-Bar
+/*		If Abs(.Height) > 500 Then ' avoiding the happening of minimun window size
+        	.ScaleHeight = -.Height ' set the x and y axial
+        	.ScaleWidth = .Width
+        	.ScaleTop = .Height
+        	' gsbgStartindex : 起始日期
+        	' gsngEndindex   : 結束日期
+        	' gsngBarwidth   : K-Bar的寬度
+        	If gsngStartIndex > 1 Then
+            	gsngStartIndex = Int(gsngEndIndex - frmEarvinStocks.Width / gsngBarWidth)
+            	Else	
+            	gsngEndIndex = gsngStartIndex + Int(frmEarvinStocks.Width / gsngBarWidth)
+            	End If
+            	Call DrawStockForm(gsngEndIndex, GetStockName(cboStocks.Text))
+            End If
+        End If
+*/        
+    	//-- 20180106 ADD END ---------------------------------
 	}
 
 
-	class DisplayStocksForm extends Canvas {
-		public void paint(Graphics g) {
-			g.drawString("Hello", 140, 140);
-			setBackground(Color.WHITE);
-			g.fillRect(130, 130, 100, 80);
-			g.drawOval(130, 130, 50, 60);
-			setForeground(Color.RED);
-			g.fillOval(130, 130, 50, 60);
-			g.drawArc(310, 200, 40, 50, 90, 60);
-			g.fillArc(310, 130, 40, 50, 180, 40);
-
-		}
+	
+	// 繪出股票圖
+	// endIndex 結束日期
+	// stockName 股票中文名稱
+	public void DrawStockForm(int endIndex, String stockName) {
+/*	    frameCount = cboFrameNum.Text ' 記錄目前的frame數目
+	    Call SetEachFrameHigh(frameCount)
+	    Call DrawOutlineOfFrames(frameCount)
+	    Call SetDisplayStartIndex
+	    Call DrawStockFormByStockType(cboStocksType.Text, displayEndIndex)
+*/
+		
 	}
+	
+	// 設定各個frame高度
+	// frameCount frame數量
+	public void SetEachFrameHigh(int frameCount) {
+/*	    Dim processFrame As Integer ' 目前準備處理的frame
+	    
+	    If frameCount = 1 Then
+	        mudtFrame(1).sngHeight = Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand - gsngBottomFrame
+	    Else
+	        mudtFrame(1).sngHeight = 3 * ((Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand - gsngBottomFrame)) / (frameCount + 2)
+	        For processFrame = 2 To frameCount
+	            mudtFrame(processFrame).sngHeight = (Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand - gsngBottomFrame) / (frameCount + 2)
+	        Next
+	    End If
+*/
+	}
+	
+	// 繪製frame外框
+	// frameCount frame數量
+	public void DrawOutlineOfFrames(int frameCount) {
+/*	    Dim processFrame As Integer
+	    ' left side
+	    Line (0.98 * gsngLeftLevel, gsngBottomFrame)-(0.98 * gsngLeftLevel, Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand), RGB(200, 100, 0)
+	    ' right side
+	    Line (Abs(frmEarvinStocks.ScaleWidth) - gsngRightLevel, gsngBottomFrame)-(Abs(frmEarvinStocks.ScaleWidth) - gsngRightLevel, Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand), RGB(200, 100, 0)
+	    ' bottom side
+	    Line (0.98 * gsngLeftLevel, 0.98 * gsngBottomFrame)-(frmEarvinStocks.ScaleWidth - 0.99 * gsngRightLevel, 0.98 * gsngBottomFrame), RGB(200, 100, 0)
+	    ' top side
+	    Line (0.98 * gsngLeftLevel, Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand)-(frmEarvinStocks.ScaleWidth - 0.99 * gsngRightLevel, Abs(frmEarvinStocks.ScaleHeight) - gsngTopFrame - gsngTopCommand), RGB(200, 100, 0)
+	    gsngYshift = gsngBottomFrame
+	    For processFrame = frameCount To 2 Step -1
+	        gsngYshift = gsngYshift + mudtFrame(processFrame).sngHeight
+	        ' draw bottom line of current frame
+	        Line (gsngLeftLevel, gsngYshift)-(frmEarvinStocks.ScaleWidth - gsngRightLevel, gsngYshift), RGB(255, 255, 255)
+	    Next
+*/
+		
+	}
+	
+	// 記錄要顯示的資料起始位置(gsngStartindex
+	public void SetDisplayStartIndex() {
+//		gsngStartIndex = IIf(Int(gsngEndIndex - frmEarvinStocks.Width / gsngBarWidth) >= 1, Int(gsngEndIndex - frmEarvinStocks.Width / gsngBarWidth), 1)
+	}
+	
+	// 繪製要顯示的線圖是 日線 OR 週線 OR 月線
+	// stockType 日線 OR 週線 OR 月線
+	// displayEndIndex
+	public void DrawStockFormByStockType(String stockType, int displayEndIndex) {
+/*	    If stockType = "日線" Then
+	        Call DrawFrameData(gudtStockDay, gudtIndexDay) ' Draw each index
+	        Call DrawStockIndexes(gudtStockDay, gudtIndexDay, gintDayIndex, displayEndIndex) ' Display each index values on right side
+	    ElseIf stockType = "週線" Then
+	        Call DrawFrameData(gudtStockWeek, gudtIndexWeek)
+	        Call DrawStockIndexes(gudtStockWeek, gudtIndexWeek, gintWeekIndex, displayEndIndex)
+	    ElseIf stockType = "月線" Then
+	        Call DrawFrameData(gudtStockMonth, gudtIndexMonth)
+	        Call DrawStockIndexes(gudtStockMonth, gudtIndexMonth, gintMonthIndex, displayEndIndex)
+	    End If
+*/
+	}
+	
+	// 判斷要繪製的資料
+	// 要傳入stockData, indexData
+	public void DrawFrameData() {
+/*	    Dim processFrame As Integer
+	    Dim frameCount As Integer
+	        
+	    frameCount = frmEarvinStocks.cboFrameNum
+	    gbytFrameNum = frameCount   ' 還不能拿掉，後面會用到…
+	    
+	    For processFrame = 1 To frameCount
+	        Select Case mudtFrame(processFrame).bytAttribute
+	            Case mKDmap
+	                Call Chalk_Line_Map(gudtStock, gudtIndex, mKDmap, 3, processFrame)
+	            Case mRSImap
+	                Call Chalk_Line_Map(gudtStock, gudtIndex, mRSImap, 3, processFrame)
+	            Case mPSYmap
+	                Call Chalk_Line_Map(gudtStock, gudtIndex, mPSYmap, 2, processFrame)
+	            Case mEMAmap
+	                Call Chalk_Line_Map(gudtStock, gudtIndex, mEMAmap, 2, processFrame)
+	            Case mBiasmap
+	                Call Chalk_Line_Map(gudtStock, gudtIndex, mBiasmap, 1, processFrame)
+	            Case mSectorMap
+	                Call Chalk_Bar_MAP(gudtStock, gudtIndex, mSectorMap, 2, processFrame)
+	            Case mMACDmap
+	                Call Chalk_MACD_Map(gudtStock, gudtIndex, processFrame)
+	            Case mQuantityMap ' 20100415 , Has problem ...
+	                Call Chalk_Quantity_Map(gudtStock, gudtIndex, processFrame)
+	         
+	            '--- 下面有空再改~~ 應該著重在預測分析模型才對!! 又不是要練寫程式說~~ (20100416) ----
+	            Case mKmap
+	                Call Chalk_K_Map(gudtStock, gudtIndex, processFrame)
+	            Case mStochRSImap
+	                Call Chalk_StochRSI_Map(gudtStock, gudtIndex, processFrame)
+	            Case mWRSImap
+	                Call Chalk_WRSI_Map(gudtStock, gudtIndex, processFrame)
+	            Case mWMSmap
+	                Call Chalk_William_Map(gudtStock, gudtIndex, processFrame)
+	            Case mRWMSmap
+	                Call Chalk_RWilliam_Map(gudtStock, gudtIndex, processFrame)
+	            Case mAccMap
+	                Call Chalk_Acc_Map(gudtStock, gudtIndex, processFrame)
+	            Case mTomeMap
+	                Call Chalk_Tome_Map(gudtStock, gudtIndex, processFrame)
+	            Case mGRGMap
+	                Call Chalk_GRG_MAP(gudtStock, gudtIndex, processFrame)
+	            Case mSignalMap
+	                Call Chalk_Signal_Map(gudtStock, gudtIndex, processFrame)
+	            Case Else
+	        End Select
+	    Next
+*/	
+		
+	}
+	
+	
 }
