3587d155984f6ec4231296bc7f93f48e86638a9a
John Lin <john.lin0908@gmail.com>
2018-01-23 10:44:52 +0800

    20180123 10:44 Write K線圖, not finish...

---
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/StockData.java
@@ -0,0 +1,22 @@
+package tw.idv.earvin.stockpgms.javaswing;
+
+public class StockData {
+	/*
+	Public Type StockData
+    sngDate As Single
+    sngStartprice As Single
+    sngHighPrice As Single
+    sngLowPrice As Single
+    sngEndprice As Single
+    sngVol As Single
+    sngAcc As Single
+    sngTome As Single
+    End Type
+    */
+	private float date;
+	private float startPrice;
+	private float highPrice;
+	private float lowPrice;
+	private float endPrice;
+	
+}
---
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/TestReadDatFile.java
@@ -0,0 +1,11 @@
+package tw.idv.earvin.stockpgms.javaswing;
+
+public class TestReadDatFile {
+	// 日期,開盤,最高,最低,收盤, 成交量, 融資張數, 融券張數
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
