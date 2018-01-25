0eca5afa4e4a41e4e0d9cfc13302e0c27d27ff3e
earvinlin <lin.earvin@gmail.com>
2018-01-25 19:38:18 +0800

    20180125 19:37 write draw frame, not finish...

--- stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/StockData.java
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/StockData.java
@@ -2,23 +2,15 @@ package tw.idv.earvin.stockpgms.javaswing;
 
 public class StockData {
 	/*
-	Public Type StockData
-    sngDate As Single
-    sngStartprice As Single
-    sngHighPrice As Single
-    sngLowPrice As Single
-    sngEndprice As Single
-    sngVol As Single
-    sngAcc As Single
-    sngTome As Single
-    End Type
-    */
-	/*
-	 select_sql = "SELECT DATE, START_PRICE, HIGH_PRICE, LOW_PRICE, END_PRICE, VOLUME, MARGIN_TRADING, SHORT_SELLING 
-	 FROM TAIWAN_DATA_POLARIS_STOCKS 
-	 WHERE STOCK_NO = %s ORDER BY DATE " 
+	 * Public Type StockData sngDate As Single sngStartprice As Single sngHighPrice
+	 * As Single sngLowPrice As Single sngEndprice As Single sngVol As Single sngAcc
+	 * As Single sngTome As Single End Type
 	 */
-
+	/*
+	 * select_sql = "SELECT DATE, START_PRICE, HIGH_PRICE, LOW_PRICE, END_PRICE,
+	 * VOLUME, MARGIN_TRADING, SHORT_SELLING FROM TAIWAN_DATA_POLARIS_STOCKS WHERE
+	 * STOCK_NO = %s ORDER BY DATE "
+	 */
 
 	private int date;
 	private double startPrice;
@@ -28,11 +20,69 @@ public class StockData {
 	private double volume;
 	private double marginTrading;
 	private double shortSelling;
-	
+
 	public double getDate() {
 		return date;
 	}
+
 	public void setDate(int v) {
 		date = v;
 	}
+
+	public double getStartPrice() {
+		return startPrice;
+	}
+
+	public void setStartPrice(double v) {
+		startPrice = v;
+	}
+
+	public double getHighPrice() {
+		return highPrice;
+	}
+
+	public void setHighPrice(double v) {
+		highPrice = v;
+	}
+
+	public double getLowPrice() {
+		return lowPrice;
+	}
+
+	public void setLowPrice(double v) {
+		lowPrice = v;
+	}
+
+	public double getEndPrice() {
+		return endPrice;
+	}
+
+	public void setEndPrice(double v) {
+		endPrice = v;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double v) {
+		volume = v;
+	}
+
+	public double getMarginTrading() {
+		return marginTrading;
+	}
+
+	public void setMarginTrading(double v) {
+		marginTrading = v;
+	}
+
+	public double getShortSelling() {
+		return shortSelling;
+	}
+
+	public void setShortSelling(double v) {
+		shortSelling = v;
+	}
+
 }
--- stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/TestReadDatFile.java
+++ stocks-modules/src/main/java/tw/idv/earvin/stockpgms/javaswing/TestReadDatFile.java
@@ -1,11 +1,25 @@
 package tw.idv.earvin.stockpgms.javaswing;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+
 public class TestReadDatFile {
 	// 日期,開盤,最高,最低,收盤, 成交量, 融資張數, 融券張數
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+		try {
+			FileReader fr = new FileReader(
+					"C:\\myData\\Dropbox\\myStocksPGMs\\V2.0\\PolarisDataToDat\\csv\\2002.csv");
+			char[] cbuf = new char[100];
+			int hasRead = 0;
+			while ((hasRead = fr.read(cbuf)) > 0) {
+				System.out.println(new String(cbuf, 0, hasRead));
+			}
+			
+			fr.close();
+		} catch (Exception e) {
+
+		}
 	}
 
 }
